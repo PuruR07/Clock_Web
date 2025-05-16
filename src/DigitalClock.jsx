72c25083f9ecc65e64eecf9cdd8b7a7f874e0d7f
Puru Raghuwanshi <pururaghuwanshi07@gmail.com>
2025-05-16 14:14:38 +0530

    Made this to understand the useEffect hook

---
+++ src/DigitalClock.jsx
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+
+function DigitalClock() {
+    const [Time, setTime] = useState(new Date());
+    useEffect(()=>{
+    const intervalId = setInterval(()=>{
+        setTime(new Date());
+    },1000);
+    return () => clearInterval(intervalId);
+    },[]);
+
+
+    function formatTime() {
+        let hours = Time.getHours();
+        let minutes = Time.getMinutes();
+        let seconds = Time.getSeconds();
+        const meridiem = hours >=12 ? "PM" : "AM";
+
+        hours = hours % 12|| 12;
+        hours = hours < 10 ? `0${hours}` : hours;
+        minutes = minutes < 10 ? `0${minutes}` : minutes;
+        seconds = seconds < 10 ? `0${seconds}` : seconds;
+        return `${hours}:${minutes}:${seconds} ${meridiem}`
+    }
+
+    return(
+        <>
+            <div className="clock-container">
+                <div className="clock">
+                    <p>Time According to IST</p>
+                    {/* <span>00:00:00</span> */}
+                    {/* <span>{Time.getHours}:{Time.getMinutes}:{Time.getSeconds}</span> */}
+                    <span>{formatTime()}</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default DigitalClock;
